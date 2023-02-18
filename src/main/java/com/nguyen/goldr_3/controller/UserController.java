76a8ee2150084ac491d7a8af03cbb9884453b337
Edward Nguyen <edwardlocnguyen@gmail.com>
2023-02-18 14:05:21 -0600

    user profile html completed

--- src/main/java/com/nguyen/goldr_3/controller/UserController.java
+++ src/main/java/com/nguyen/goldr_3/controller/UserController.java
@@ -1,9 +1,17 @@
 package com.nguyen.goldr_3.controller;
 
+import com.nguyen.goldr_3.model.User;
 import com.nguyen.goldr_3.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/users/{userId}")
@@ -12,6 +20,27 @@ public class UserController {
     @Autowired
     private UserServices userServices;
 
+//    fxn to get the user's age
+    public int calculateUserAge(User _user) {
+        LocalDate dob = _user.getDob();
+        int age = (int) ChronoUnit.YEARS.between(dob, LocalDate.now());
+        return age;
+    }
 
+    @GetMapping("/profile")
+    public String profile(@PathVariable("userId") Integer userId, Model model) {
+
+//        get user data
+        Optional<User> user = userServices.getUserById(userId);
+        User _user = user.get();
+        int userAge = calculateUserAge(_user);
+
+        model.addAttribute("user", _user);
+        model.addAttribute("userId", userId.toString());
+        model.addAttribute("userAge", userAge);
+
+        return "profile";
+
+    }
 
 }
