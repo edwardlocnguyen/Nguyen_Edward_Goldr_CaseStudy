1d9a38622b7de8bc78887c5522eca0316af1ea8e
Edward Nguyen <edwardlocnguyen@gmail.com>
2023-02-18 13:33:54 -0600

    created dummy data, all model obj creations and relationships work

--- src/main/java/com/nguyen/goldr_3/Goldr3Application.java
+++ src/main/java/com/nguyen/goldr_3/Goldr3Application.java
@@ -1,6 +1,12 @@
 package com.nguyen.goldr_3;
 
+import com.nguyen.goldr_3.model.Account;
+import com.nguyen.goldr_3.model.Category;
+import com.nguyen.goldr_3.model.Entry;
 import com.nguyen.goldr_3.model.User;
+import com.nguyen.goldr_3.repository.AccountRepo;
+import com.nguyen.goldr_3.repository.CategoryRepo;
+import com.nguyen.goldr_3.repository.EntryRepo;
 import com.nguyen.goldr_3.repository.UserRepo;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +22,12 @@ public class Goldr3Application {
 
 	@Autowired
 	private UserRepo userRepo;
+	@Autowired
+	private CategoryRepo categoryRepo;
+	@Autowired
+	private AccountRepo accountRepo;
+	@Autowired
+	private EntryRepo entryRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Goldr3Application.class, args);
@@ -35,6 +47,26 @@ public class Goldr3Application {
 		user1.setDob(LocalDate.of(1994, 11, 6));
 		user1.setCreditScore(750);
 		userRepo.save(user1);
+
+//		create category
+		Category category1 = new Category();
+		category1.setName("Cash");
+		category1.setUser(user1);
+		categoryRepo.save(category1);
+
+//		create account
+		Account account1 = new Account();
+		account1.setName("BOA Checking");
+		account1.setUser(user1);
+		account1.setCategory(category1);
+		accountRepo.save(account1);
+
+//		create entry
+		Entry entry1 = new Entry();
+		entry1.setAmount(1000.01);
+		entry1.setDate(LocalDate.now());
+		entry1.setAccount(account1);
+		entryRepo.save(entry1);
 
 	}
 
