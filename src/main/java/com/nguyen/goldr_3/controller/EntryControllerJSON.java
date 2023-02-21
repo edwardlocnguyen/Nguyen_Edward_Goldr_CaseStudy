192060c8f4744771fa03c305b8eef527a2a0dc44
Edward Nguyen <edwardlocnguyen@gmail.com>
2023-02-21 16:33:57 -0600

    cleaned up API

--- src/main/java/com/nguyen/goldr_3/controller/EntryControllerJSON.java
+++ src/main/java/com/nguyen/goldr_3/controller/EntryControllerJSON.java
@@ -5,7 +5,7 @@ import com.nguyen.goldr_3.services.EntryServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping
@@ -14,10 +14,32 @@ public class EntryControllerJSON {
     @Autowired
     private EntryServices entryServices;
 
+    // fxn used in getAllEntries method
+    public List<Map<String, Object>> convertEntriesToMap(List<Entry> allEntries) {
+        List<Map<String, Object>> result = new ArrayList<>();
+
+        for (Entry entry : allEntries) {
+            Map<String, Object> entryMap = new LinkedHashMap<>();
+            entryMap.put("entryId", entry.getId());
+            entryMap.put("entryAmount", entry.getAmount());
+            entryMap.put("entryDate", entry.getDate());
+            entryMap.put("entry_AccountCategoryId", entry.getAccount().getCategory().getId());
+            entryMap.put("entry_AccountCategoryName", entry.getAccount().getCategory().getName());
+            entryMap.put("entry_UserCreditScore", entry.getUser().getCreditScore());
+            entryMap.put("entry_UserDob", entry.getUser().getDob());
+
+            result.add(entryMap);
+        }
+
+        return result;
+    }
+
+
     //    used for API page
     @GetMapping("entries/api-data")
-    public List<Entry> getAllEntries() {
-        return entryServices.getAllEntries();
+    public List<Map<String, Object>> getAllEntries() {
+        List<Entry> allEntries = entryServices.getAllEntries();
+        return convertEntriesToMap(allEntries);
     }
 
     @GetMapping("users/{userId}/entries/api-data")
